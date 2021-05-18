5d06244c682ca6be507f6e159b9e2fc84e85900f
Akilax0 <killerilaksha@gmail.com>
2021-05-18 20:12:07 +0530

    patr1

--- mnist_ml/src/data_handler.cc
+++ mnist_ml/src/data_handler.cc
@@ -149,6 +149,7 @@ std::vector<data *> *data_handler::get_validation_data(){
   return validation_data;
 }
 
+/*
 int main(){
   data_handler *dh = new data_handler();
   dh->read_feature_vector("./train-images.idx3-ubyte");
@@ -156,3 +157,4 @@ int main(){
   dh->split_data();
   dh->count_classes();
 }
+*/
